abdd501baafa5c60c632c3f6308a15a8b768199f
John Fevre <fevs11@gmail.com>
2020-04-04 23:01:13 +0100

    rework ssh to playbook

--- Manjaro-laptop-Setup/Enable-ssh.sh
+++ Manjaro-laptop-Setup/Enable-ssh.sh
@@ -1,15 +1,42 @@
-sudo pacman -S openssh
+- name: Update & upgrade pacman
+  pacman:
+    update_cache: yes
+    upgrade: yes
 
-sudo systemctl enable sshd
+-name: Install Openssh
+  pacman:
+    name:
+      - openssh
+    state: Present
 
-sudo systemctl start sshd
 
-file=/etc/ssh/sshd_config
-sudo cp -p $file $file.old &&
-awk '
-$1=="PermitRootLogin" {$2="yes"}
-$1=="PasswordAuthentication" {$2="yes"}
-{print}
-' $file.old > $file
+- name: enable service httpd and ensure it is not masked
+  systemd:
+    name: openssh
+    enabled: yes
 
-sudo systemctl restart sshd
+- name: Make sure a service is running
+  systemd:
+    state: started
+    name: openssh
+
+#-name: Allow Root Login
+   lineinfile:
+    path:/etc/ssh/sshd_config
+    backup: yes
+    regexp: 'PermitRootLogin'
+    line: PermitRootLogin yes
+#-name: Allow Root Password Auth
+   lineinfile:
+    path:/etc/ssh/sshd_config
+    backup: yes
+    regexp: 'PasswordAuthentication'
+    line: PasswordAuthentication yes
+- name: Restart openssh service
+  systemd:
+    state: restarted
+    name: openssh
+
+
+
+
